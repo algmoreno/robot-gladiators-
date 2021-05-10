042858850cd239cc409f80e6f459f329bd055810
algmoreno <alg.moreno00@gmail.com>
2021-05-10 12:19:39 -0700

    added enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,19 @@
 var playerName=window.prompt("What is your robot's name?");
 var playerHealth=100;
 var playerAttack=10;
+var playerMoney= 10;
 
-var enemyName="Roborto";
+var enemyNames =["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth=50;
 var enemyAttack=12;
 
-var playerMoney= 10;
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + " index");
+}
 
-var fight = function()  {
+var fight = function(enemyName)  {
     window.alert("Welcome to Robot Gladiators!");
 
 
@@ -55,4 +60,8 @@ else  {
     window.alert("You need to choose a valid option. Try again!");
 }
 }
-fight(); 
+
+
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
